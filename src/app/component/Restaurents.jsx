e9fe6a0ab245589da18f4d71fbcb6cfe4ea3fbf2
fradi sirine <fradisirine04@gmail.com>
2024-05-03 13:04:25 +0100

    updates

--- src/app/component/Restaurents.jsx
+++ src/app/component/Restaurents.jsx
@@ -2,20 +2,20 @@ import Head from 'next/head';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStar } from '@fortawesome/free-solid-svg-icons'
 
-const Restaurant = ({ name, address, logo, link, rating }) => (
+const Restaurant = ({ name, address, logo, link}) => (
   <div className="max-w-md mx-auto shadow-lg p-8 mb-8 bg-white">
-    <img src={logo} alt={`${name} logo`} className="w-full mb-4" />
+    <link href={link} ><img src={logo} alt={`${name} logo`}  className="w-full mb-4" /></link>
     <fonction> Dining & Delivery
     </fonction>
     <h2 className="text-2xl font-bold mb-2">{name}</h2>
     <p className="text-gray-700">{address}</p>
     <div className="flex items-center mt-2">
-      <p className="text-gray-600 mr-2">Rating:</p>
+    
       {Array.from({ length: rating }, (_, index) => (
         <FontAwesomeIcon key={index} icon={faStar} className="text-yellow-500 p-1 text-xs" />
       ))}
     </div>
-    <a href={link} className="block mt-4 text-blue-500">Visit Website</a>
+    
   </div>
 );
 
@@ -25,56 +25,56 @@ const restaurantData = [
     address: '123 Main St, City, Country',
     logo: '/bagette.png',
     link: '/Baguette et baguette',
-    rating: 4
+ 
   },
   {
     name: 'Chaneb Plus Sahloul',
     address: '456 Elm St, City, Country',
     logo: '/chaneb.png',
     link: '/Chaneb Plus Sahloul',
-    rating: 4
+   
   },
   {
     name: 'Crépes Factory',
     address: '456 Elm St, City, Country',
     logo: '/factory.png',
     link: '/Crépes Factory',
-    rating: 5
+   
   },
   {
     name: 'KFC ',
     address: '456 Elm St, City, Country',
     logo: '/mac.png',
     link: '/KFC',
-    rating: 4
+   
   },
   {
     name: 'Papa Jhons',
     address: '789 Oak St, City, Country',
     logo: '/papa.png',
     link: '/Papa Jhons',
-    rating: 5
+    
   },
   {
     name: 'Plan B',
     address: '101 Pine St, City, Country',
     logo: '/plan.png',
     link: '/Plan B',
-    rating: 5
+   
   },
   {
     name: 'Spart’n Tacos',
     address: '111 Maple St, City, Country',
     logo: '/spartan.png',
     link: '/Spart’n Tacos',
-    rating: 4
+  
   },
   {
     name: 'Tabouna Food 8',
     address: '222 Spruce St, City, Country',
     logo: '/tabouna.png',
     link: '/Tabouna Food',
-    rating: 4
+    
   },
 ];
 
@@ -103,7 +103,6 @@ const Restaurants = () => {
                 address={restaurant.address}
                 logo={restaurant.logo}
                 link={restaurant.link}
-                rating={restaurant.rating}
               />
             </div>
           ))}
